c776fd6f5a405bc04e0d109970b2e0116b298fcc
TriTueDang <145659919+TriTueDang@users.noreply.github.com>
2026-03-02 06:53:46 +0100

    Feature/start (#2)
    * starting setup, without opencv
    
    * added opencv lib
    
    * update gitignore
    
    * ignore CMakePresets
    
    * update gitignore
    
    * setup project
    
    * includes
    
    * update cv02
    
    * Fix build for Linux: comment wglew.h include
    
    * app
    
    * this works
    
    * refactor init and run
    
    * added time measure
    
    * Implementace GLFW callbacků pro interakci, dynamické barvy trojúhelníku a pozadí, aktualizace debug callbacku OpenGL a odstranění wglew.
    
    * headers and link lib
    
    * task 4, vsync and json
    
    * Aktualizována konfigurace CMake a vcpkg pro detekci a propojení OpenGL a OpenCV.
    
    * Odstraněny dočasné sestavovací soubory z repozitáře
    
    * Přidáno ignorování adresáře `/build_new/` v `.gitignore`.
    
    * Přidány skripty `start.sh` a `start.bat` pro sestavení a spuštění projektu na různých operačních systémech.
    
    * Optimalizace: OpenCV odstraněno z vcpkg (zrychlení buildu)
    
    * Přidána logika pro opakování konfigurace CMake s vyčištěním adresáře sestavení v případě selhání.
    
    * Aktualizovány vzory pro ignorování laboratorních souborů v .gitignore.
    
    * Přidána funkce pro přepínání celoobrazovkového režimu klávesou 'F'.
    
    * Přidáno vynucení použití NVIDIA GPU pro spuštění aplikace na Linuxu.
    
    * Skrýt okno během inicializace a zobrazit ho po načtení všech zdrojů.
    
    * cv3, task1.1, task1.2
    
    * Add ImGUI dependency to vcpkg.json
    
    * Fix build errors after merge (variable name and syntax)
    
    * Přidává počáteční zachycení myši, vylepšuje přepínání celoobrazovkového režimu a refaktoruje názvy proměnn
    
    ---------
    
    Co-authored-by: Jakub Marcinka <jakub.marcinka@tul.cz>

---
+++ main.cpp
@@ -0,0 +1,70 @@
+// icp.cpp
+// author: JJ
+
+#include <iostream>
+#include <chrono>
+#include <stack>
+#include <random>
+
+// OpenCV (does not depend on GL)
+#if __has_include(<opencv2/opencv.hpp>)
+    #include <opencv2/opencv.hpp>
+#elif __has_include(<opencv4/opencv2/opencv.hpp>)
+    #include <opencv4/opencv2/opencv.hpp>
+#else
+    #error "OpenCV hlavičkové soubory nebyly nalezeny!"
+#endif
+
+// OpenGL Extension Wrangler: allow all multiplatform GL functions
+#include <GL/glew.h>
+// WGLEW = Windows GL Extension Wrangler (change for different platform)
+// platform specific functions (in this case Windows)
+
+// GLFW toolkit
+// Uses GL calls to open GL context, i.e. GLEW __MUST__ be first.
+#include <GLFW/glfw3.h>
+
+// OpenGL math (and other additional GL libraries, at the end)
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "app.hpp"
+// define our application
+App app;
+
+int main()
+{
+    using clock = std::chrono::steady_clock;
+    using seconds = std::chrono::duration<double>;
+
+    auto start_time = clock::now();
+
+    try {
+        if (app.init()) {
+            int result = app.run();
+
+            auto end_time = clock::now();
+            seconds elapsed = end_time - start_time;
+
+            std::cout << "Application runtime: "
+                      << elapsed.count()
+                      << " seconds" << std::endl;
+
+            return result;
+        }
+    }
+    catch (std::exception const& e) {
+        std::cerr << "App failed : " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    auto end_time = clock::now();
+    seconds elapsed = end_time - start_time;
+
+    std::cout << "Application runtime: "
+              << elapsed.count()
+              << " seconds" << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
